e4ec6d7dcf77bfe4a54254dfdfc5ee8afd110c21
Pamelaleo96 <delacruzlozano.p@gmail.com>
2017-11-21 15:21:40 -0500

    Añadiendo apuntes

--- js/app.js
+++ js/app.js
@@ -13,3 +13,24 @@ function createList(event) {
 function begin() {
   addList.addEventListener('click', createList)
 }
+/*function begin() {
+  addList.addEventListener('click', newList)
+  button.addEventListener('click', createList)
+}
+//verificamos si hay texto en el input
+function createList(){
+  if(nameList.value){
+    form.classList.toggle('display-none'); //a todas las clase displaynone, oculta
+    var div = document.createElement('a');
+    a.innerHTML = nameList.value;
+  }
+}*/
+
+//REPASO metodos que esperan que disparen el load(evento)
+/*1° window.addEventListener('load', function(){ //crea un evento
+});
+2° window.addEventListener('load', begin) //creas un evento 
+function begin(){ //funcion anonima
+}
+3° window.onload = begin; //llamas al evento
+4° window.load = function(){} //crea un evento */
